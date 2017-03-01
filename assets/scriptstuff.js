c0c901947b74779b82ba6f4b2664cdcbd62937f0
Daniel Garcia <danielegarciav@gmail.com>
2017-02-28 19:31:14 -0500

    lots of things

---
+++ assets/scriptstuff.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", dothething, false);
+
+function dothething(){
+	var herobg = document.querySelector('[title="hero"]');
+	herobg.id = "herobg";
+	//document.getElementById("herodiv").appendChild(herobg);
+	//herobg.parentNode.removeChild(herobg)
+}
+
+function wow(){
+	alert("stop");
+}
